b5e5196d92fb1ed6f7184ec9fe724b72f1523252
Mubarak Ganiyu <ganiyubaraq@gmail.com>
2023-09-05 21:23:16 -0500

    Update audio.py

--- audio.py
+++ audio.py
@@ -1,13 +1,25 @@
 import streamlit as st
 from audio_recorder_streamlit import audio_recorder
+from transformers import pipeline
+import torch 
+#import librosa
+#import soundfile
+
+checkpoint = "openai/whisper-small.en"  
+
+@st.cache(allow_output_mutation=True)
+def model():
+    pipe = pipeline("automatic-speech-recognition", model=checkpoint)
+    return pipe
 
 audio_bytes = audio_bytes = audio_recorder(
     text="",
     recording_color="#e8b62c",
     neutral_color="#6aa36f",
     icon_name="user",
-    icon_size="6x",
+    icon_size="3x",
 )
 
 if audio_bytes:
     st.audio(audio_bytes, format="audio/wav")
+    st.write(pipe(audio_bytes)["text"])
