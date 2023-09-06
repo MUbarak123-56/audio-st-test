5548758e63877a43100faf77bdd1c920b665aa66
Mubarak Ganiyu <ganiyubaraq@gmail.com>
2023-09-05 22:24:44 -0500

    Update audio.py

--- audio.py
+++ audio.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from audio_recorder_streamlit import audio_recorder
+from audiorecorder import audiorecorder
 from transformers import pipeline
 from transformers import WhisperProcessor, WhisperForConditionalGeneration
 import torch 
@@ -16,21 +16,28 @@ def model():
     #model.config.forced_decoder_ids = None
     return pipe #processor, model
 
-audio_bytes = audio_recorder(
-    text="",
-    recording_color="#e8b62c",
-    neutral_color="#6aa36f",
-    icon_name="user",
-    icon_size="3x",
-)
+#audio_bytes = audio_recorder(    text="",    recording_color="#e8b62c",   neutral_color="#6aa36f",   icon_name="user",   icon_size="3x",)
 
 #processor, model = model()
 
-if audio_bytes:
-    st.audio(audio_bytes, format="audio/wav")
+audio = audiorecorder("Click to record", "Click to stop recording")
+
+if not audio.empty():
+    # To play audio in frontend:
+    st.audio(audio.export().read())  
+
+    # To save audio to a file, use pydub export method:
+    audio.export("audio.wav", format="wav")
+
+    # To get audio properties, use pydub AudioSegment properties:
+    st.write(f"Frame rate: {audio.frame_rate}, Frame width: {audio.frame_width}, Duration: {audio.duration_seconds} seconds")
+
+
+#if audio_bytes:
+ #   st.audio(audio_bytes, format="audio/wav")
     #input_features = processor(sample["array"], sampling_rate=sample["sampling_rate"], return_tensors="pt").input_features 
     # generate token ids
     #predicted_ids = model.generate(input_features)
     #transcription = processor.batch_decode(predicted_ids, skip_special_tokens=True)
     #st.write(pipe(audio_bytes)["text"])
-    st.write(audio_bytes)
+  #  st.write(audio_bytes)
