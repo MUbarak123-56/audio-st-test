1ccb32d4e66de9b4c7ab8704d3453fb34b583938
Mubarak Ganiyu <ganiyubaraq@gmail.com>
2023-09-06 21:16:21 -0500

    Update audio.py

--- audio.py
+++ audio.py
@@ -23,13 +23,14 @@ pipe = model()
 if audio_bytes:
     new_audio = st.audio(audio_bytes, format="audio/wav")
     #audio_np = np.array(new_audio)
-    data_s16 = np.frombuffer(audio_bytes, dtype=np.int16) #count=len(audio_bytes)//2, offset=0)
-    float_data = data_s16 * 0.5**15
+    #data_s16 = np.frombuffer(audio_bytes, dtype=np.int16) #count=len(audio_bytes)//2, offset=0)
+    #float_data = data_s16 * 0.5**15
     bytes_io = BytesIO(audio_bytes)
     
     # Read the file sample rate and data using wavfile
     sample_rate, audio_data = wavfile.read(audio_bytes)
-    audio_input = {"array": audio_data[:,0].astype(np.float32)*(1/32768.0), "sampling_rate": sample_rate}
+    audio_input = {"array": np.frombuffer(audio_data, np.int16).flatten().astype(np.float32) / 32768.0 #audio_data[:,0].astype(np.float32)*(1/32768.0), 
+                   "sampling_rate": sample_rate}
     #audio_input["array"] = np.array(float_data)
     #audio_input["sampling_rate"] = 16000
     #input_features = processor(sample["array"], sampling_rate=sample["sampling_rate"], return_tensors="pt").input_features 
