60e496e29e46797537cc5380f7fddf6e3659c4d1
Mubarak Ganiyu <ganiyubaraq@gmail.com>
2023-09-07 10:41:00 -0500

    Update audio.py

--- audio.py
+++ audio.py
@@ -17,7 +17,7 @@ def model():
     model = WhisperForConditionalGeneration.from_pretrained(checkpoint)
     return pipe, processor, model
 
-audio_bytes = audio_recorder(text="Click Me", recording_color="#e8b62c", neutral_color="#6aa36f", icon_name="user", icon_size="1x")
+audio_bytes = audio_recorder(text="Click Me", recording_color="#e8b62c", neutral_color="#6aa36f", icon_name="user", icon_size="1x", sample_rate = 16_000)
 
 #pipe = model()
 pipe, processor, model = model()
@@ -28,7 +28,7 @@ if audio_bytes:
     
     # Read the file sample rate and data using wavfile
     sample_rate, audio_data = wavfile.read(bytes_io)
-    audio_input = {"array": np.frombuffer(audio_data, np.int16).flatten().astype(np.float32) / 32768.0, #audio_data[:,0].astype(np.float32)*(1/32768.0), 
+    audio_input = {"array": np.frombuffer(audio_data, np.int16).flatten().astype(np.float32)/32768.0, #audio_data[:,0].astype(np.float32)*(1/32768.0), 
                    "sampling_rate": sample_rate}
     
     st.write(audio_input)
