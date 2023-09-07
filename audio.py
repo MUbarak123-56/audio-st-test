d9efa6606ac1e73fb13454f46fc8002cf8b04498
Mubarak Ganiyu <ganiyubaraq@gmail.com>
2023-09-06 20:42:52 -0500

    Update audio.py

--- audio.py
+++ audio.py
@@ -3,6 +3,7 @@ from transformers import pipeline
 from transformers import WhisperProcessor, WhisperForConditionalGeneration
 from audio_recorder_streamlit import audio_recorder
 import numpy as np
+from scipy.io import wavfile
 #import librosa
 #import soundfile
 
@@ -23,7 +24,11 @@ if audio_bytes:
     #audio_np = np.array(new_audio)
     data_s16 = np.frombuffer(audio_bytes, dtype=np.int16) #count=len(audio_bytes)//2, offset=0)
     float_data = data_s16 * 0.5**15
-    audio_input = {"array": np.array(float_data), "sampling_rate": 16000}
+    bytes_io = BytesIO(audio_bytes)
+    
+    # Read the file sample rate and data using wavfile
+    sample_rate, audio_data = wavfile.read(bytes_io)
+    audio_input = {"array": audio_data, "sampling_rate": 16000}
     #audio_input["array"] = np.array(float_data)
     #audio_input["sampling_rate"] = 16000
     #input_features = processor(sample["array"], sampling_rate=sample["sampling_rate"], return_tensors="pt").input_features 
