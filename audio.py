19168f0d2ebf8c3bd20b7595daa58eab3eb79158
Mubarak Ganiyu <ganiyubaraq@gmail.com>
2023-09-06 22:18:10 -0500

    Update audio.py

--- audio.py
+++ audio.py
@@ -12,15 +12,15 @@ checkpoint = "openai/whisper-small.en"
 
 @st.cache(allow_output_mutation=True)
 def model():
-    pipe = pipeline("automatic-speech-recognition", model=checkpoint)
-    #processor = WhisperProcessor.from_pretrained(checkpoint)
-    #model = WhisperForConditionalGeneration.from_pretrained(checkpoint)
-    return pipe#, processor, model
+    #pipe = pipeline("automatic-speech-recognition", model=checkpoint)
+    processor = WhisperProcessor.from_pretrained(checkpoint)
+    model = WhisperForConditionalGeneration.from_pretrained(checkpoint)
+    return processor, model
 
 audio_bytes = audio_recorder(text="Click Me", recording_color="#e8b62c", neutral_color="#6aa36f", icon_name="user", icon_size="1x", sample_rate = 16_000)
 
-pipe = model()
-#, processor, model = model()
+#pipe = model()
+processor, model = model()
 
 if audio_bytes:
     new_audio = st.audio(audio_bytes, format="audio/wav")
@@ -34,13 +34,13 @@ if audio_bytes:
     audio_input = {"array": np.frombuffer(audio_data, np.int16).flatten().astype(np.float32) / 32768.0, #audio_data[:,0].astype(np.float32)*(1/32768.0), 
                    "sampling_rate": sample_rate}
     
-    #input_features = processor(audio_input["array"], sampling_rate=audio_input["sampling_rate"], return_tensors="pt").input_features 
+    input_features = processor(audio_input["array"], sampling_rate=audio_input["sampling_rate"], return_tensors="pt").input_features 
     
-    #predicted_ids = model.generate(input_features)
+    predicted_ids = model.generate(input_features)
     # decode token ids to text
-    #transcription = processor.batch_decode(predicted_ids, skip_special_tokens=True)
-    #st.write(transcription)
+    transcription = processor.batch_decode(predicted_ids, skip_special_tokens=True)
     
     st.write(audio_input)
-    st.write(pipe(audio_input)["text"])
+    st.write(transcription)
+    #st.write(pipe(audio_input)["text"])
     
