b0bffe09cd1ba774194a2651a37971e1529c4122
Mubarak Ganiyu <ganiyubaraq@gmail.com>
2023-09-07 13:49:55 -0500

    Update audio.py

--- audio.py
+++ audio.py
@@ -15,12 +15,12 @@ def model():
     pipe = pipeline("automatic-speech-recognition", model=checkpoint)
     processor = WhisperProcessor.from_pretrained(checkpoint)
     model = WhisperForConditionalGeneration.from_pretrained(checkpoint)
-    return pipe#, processor, model
+    return pipe, processor, model
 
 audio_bytes = audio_recorder(text="Click Me", recording_color="#e8b62c", neutral_color="#6aa36f", icon_name="user", icon_size="1x")
 
-pipe = model()
-#pipe, processor, model = model()
+#pipe = model()
+pipe, processor, model = model()
 
 if audio_bytes:
     new_audio = st.audio(audio_bytes, format="audio/wav")
@@ -32,17 +32,17 @@ if audio_bytes:
     #"sampling_rate": sample_rate}
     
     audio_input = {"array": audio_data[:,0].astype(np.float32)*(1/32768.0), #audio_data[:,0].astype(np.float32)*(1/32768.0), 
-                   "sampling_rate": 16000}
+                   "sampling_rate": sample_rate}
     st.write(audio_input)
     st.write(pipe(audio_input)["text"])
     
-    #input_features = processor(audio_input["array"], sampling_rate=16000, return_tensors="pt").input_features 
+    input_features = processor(audio_input["array"], sampling_rate=16000, return_tensors="pt").input_features 
     
-    #predicted_ids = model.generate(input_features)
+    predicted_ids = model.generate(input_features)
     # decode token ids to text
-    #transcription = processor.batch_decode(predicted_ids, skip_special_tokens=True)
+    transcription = processor.batch_decode(predicted_ids, skip_special_tokens=True)
     
     
-    #st.write(transcription)
+    st.write(transcription)
     
     
