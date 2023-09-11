f3d38d9841d61b3aebdf5e99067f048c990cb3a9
Mubarak Ganiyu <ganiyubaraq@gmail.com>
2023-09-10 21:03:02 -0500

    Update audio.py

--- audio.py
+++ audio.py
@@ -49,7 +49,7 @@ def generate_response(input_query):
         #{"role": "user", "content": "Where was it played?"}
     ]
   )
-  return st.info(response["content"])
+  return st.info(response)
 
 if audio_bytes:
     new_audio = st.audio(audio_bytes, format="audio/wav")
