c7092ec0972ae5b02e85a144a69b0c1f8cd10ac5
Mubarak Ganiyu <ganiyubaraq@gmail.com>
2023-09-21 21:35:16 -0500

    Update audio2.py

--- audio2.py
+++ audio2.py
@@ -8,4 +8,4 @@ seconds = 3  # Duration of recording
 myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=2)
 sd.wait()  # Wait until recording is finished
 #write('output.wav', fs, myrecording)  # Save as WAV file 
-st.audio(myrecording, sample_rate=16000, "audio/wav")
+st.audio(myrecording, sample_rate=16000, format="audio/wav")
