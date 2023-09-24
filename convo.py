4005c6790cd1e62af9f55f5c4288e162be6098c3
Mubarak Ganiyu <ganiyubaraq@gmail.com>
2023-09-24 15:33:26 -0500

    Update convo.py

--- convo.py
+++ convo.py
@@ -38,15 +38,16 @@ def clear_chat_history():
   
 st.sidebar.button('Clear Chat History', on_click=clear_chat_history)
 
-
-if "messages" not in st.session_state.keys():
-    st.session_state.messages = [{"role": "assistant", "content": "How may I assist you today?"}]
+initial_message = [{"role": "assistant", "content": "How may I assist you today?"}]
+st.session_state.messages.append(initial_message)
 
 # Display or clear chat messages
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.write(message["content"])
-        
+
+
+
 if prompt := st.chat_input():
     st.session_state.messages.append({"role": "user", "content": prompt})
     with st.chat_message("user"):
@@ -63,6 +64,8 @@ def generate_response(input_query):
   )
   return response
 
+while True:
+    
 if st.session_state.messages[-1]["role"] != "assistant":
     with st.chat_message("assistant"):
         with st.spinner("Thinking..."):
