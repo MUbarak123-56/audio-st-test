0b87e0a83e945135502fcebb1094eedc6ee72dd0
Mubarak Ganiyu <ganiyubaraq@gmail.com>
2023-09-24 14:39:26 -0500

    Create convo.py

---
+++ convo.py
@@ -0,0 +1,59 @@
+import streamlit as st
+from transformers import pipeline
+from transformers import WhisperProcessor, WhisperForConditionalGeneration
+from audio_recorder_streamlit import audio_recorder
+import numpy as np
+from scipy.io import wavfile
+from io import BytesIO
+from langchain.chat_models import ChatOpenAI
+from langchain.llms import OpenAI
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
+import openai
+from transformers import SpeechT5Processor, SpeechT5HifiGan, SpeechT5ForTextToSpeech
+from datasets import load_dataset
+import torch
+from IPython.display import Audio
+import os
+import base64
+
+#Audio(speech, rate=16000)
+#import librosa
+with st.sidebar:
+    st.title('🦙💬 Llama 2 Chatbot')
+    if 'OPENAI_API_TOKEN' in st.secrets:
+        st.success('API key already provided!', icon='✅')
+        openai_api_key = st.secrets['OPENAI_API_TOKEN']
+    else:
+        openai_api_key = st.text_input('Enter OpenAI API token:', type='password')
+        if not (replicate_api.startswith('sk-'):
+            st.warning('Please enter your credentials!', icon='⚠️')
+        else:
+            st.success('Proceed to entering your prompt message!', icon='👉')
+
+openai.api_key = openai_api_key
+
+def generate_response(input_query):
+  #llm = OpenAI(model_name='gpt-4', temperature=0.1, openai_api_key=openai_api_key)
+  #llm2 = ChatOpenAI(model_name='gpt-4', temperature=0.1, openai_api_key=openai_api_key)
+  #prompt = PromptTemplate(
+  #  input_variables=[input_query],
+  #  template=input_query,
+  #)
+    
+  #chain = LLMChain(llm=llm, prompt=prompt)
+  response = openai.ChatCompletion.create(
+    model="gpt-4",
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": input_query},
+        #{"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
+        #{"role": "user", "content": "Where was it played?"}
+    ]
+  )
+  return response["choices"][0]["message"]["content"]
+  
+def clear_chat_history():
+    st.session_state.messages = [{"role": "assistant", "content": "How may I assist you today?"}]
+  
+st.sidebar.button('Clear Chat History', on_click=clear_chat_history)
