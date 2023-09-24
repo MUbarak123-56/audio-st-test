88cfa67c6a30d6ef0b10c3ab4468c2377a482e06
Mubarak Ganiyu <ganiyubaraq@gmail.com>
2023-09-24 16:11:21 -0500

    Update convo.py

--- convo.py
+++ convo.py
@@ -64,10 +64,12 @@ if "messages" not in st.session_state.keys():
     with st.chat_message(initial_message["role"]):
         st.write(initial_message["content"])
 
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
+
 if prompt := st.chat_input():
     st.session_state.messages.append({"role": "user", "content": prompt})
-    with st.chat_message("user"):
-        st.write(prompt)
 
 if st.session_state.messages[-1]["role"] != "assistant":
     with st.chat_message("assistant"):
@@ -81,5 +83,4 @@ if st.session_state.messages[-1]["role"] != "assistant":
             placeholder.markdown(full_response)
     message = {"role": "assistant", "content": full_response}
     st.session_state.messages.append(message)
-    with st.chat_message(st.session_state.messages[-1]["role"]):
-        st.write(st.session_state.messages[-1]["content"])
+
