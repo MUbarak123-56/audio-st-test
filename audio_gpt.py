d28e0429a93e0bc0718ea42a129899a969bad985
Mubarak Ganiyu <ganiyubaraq@gmail.com>
2023-09-24 19:10:12 -0500

    Update audio_gpt.py

--- audio_gpt.py
+++ audio_gpt.py
@@ -72,7 +72,7 @@ with st.sidebar:
         llm = 'gpt-3.5-turbo'
     elif selected_model == 'GPT 4':
         llm = 'gpt-4'
-    temp = st..number_input('temperature', min_value=0.01, max_value=4.0, value=0.1, step=0.01)
+    temp = st.number_input('temperature', min_value=0.01, max_value=4.0, value=0.1, step=0.01)
     top_percent = st.number_input('top_p', min_value=0.01, max_value=1.0, value=0.9, step=0.01)
     input_format = st.selectbox("Choose an input format", ["text", "audio"])
 
