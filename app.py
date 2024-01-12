12857fce9d02d206fb83c2902e75b2e67055dea4
Mubarak Ganiyu <ganiyubaraq@gmail.com>
2024-01-12 16:21:10 -0600

    Update app.py

--- app.py
+++ app.py
@@ -119,7 +119,7 @@ def generate_llm_response():
   for i in range(len(st.session_state.messages)):
       use_messages.append({"role": st.session_state.messages[i]["role"], "content": st.session_state.messages[i]["content"]})
 
-  response = client.chat.completions.create((
+  response = client.chat.completions.create(
     model=llm,
     messages=use_messages,
     temperature = temp,
